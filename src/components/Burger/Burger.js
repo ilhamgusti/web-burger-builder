474f6f2dd2f58e100d8d288d47d739491904821a
ilhamgusti <ilhamgewwe@gmail.com>
2019-08-18 07:56:51 +0700

    input ingredient dynamics using state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,13 +2,16 @@ import React from 'react'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 import classes from './Burger.module.css'
 
-const Burger = () => {
+const Burger = props => {
+	const transformIngredients = Object.keys(props.ingredients).map(igKey => {
+		return [...Array(props.ingredients[igKey])].map((_, i) => {
+			return <BurgerIngredient key={igKey + i} type={igKey} />
+		})
+	})
 	return (
 		<div className={classes.Burger}>
 			<BurgerIngredient type="bread-top" />
-			<BurgerIngredient type="salad" />
-			<BurgerIngredient type="cheese" />
-			<BurgerIngredient type="meat" />
+			{transformIngredients}
 			<BurgerIngredient type="bread-bottom" />
 		</div>
 	)
