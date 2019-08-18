3aa12df419c2c222b5951559f8d6be7ea8018fbf
ilhamgusti <ilhamgewwe@gmail.com>
2019-08-18 08:04:39 +0700

    flatten array if empty

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,15 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 import classes from './Burger.module.css'
 
 const Burger = props => {
-	const transformIngredients = Object.keys(props.ingredients).map(igKey => {
-		return [...Array(props.ingredients[igKey])].map((_, i) => {
-			return <BurgerIngredient key={igKey + i} type={igKey} />
+	const transformIngredients = Object.keys(props.ingredients)
+		.map(igKey => {
+			return [...Array(props.ingredients[igKey])].map((_, i) => {
+				return <BurgerIngredient key={igKey + i} type={igKey} />
+			})
 		})
-	})
+		.reduce((arr, el) => {
+			return arr.concat(el)
+		}, [])
 	return (
 		<div className={classes.Burger}>
 			<BurgerIngredient type="bread-top" />
